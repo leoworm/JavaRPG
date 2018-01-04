046e5d20b7bba7f14b1e47de3135cbbedcaab571
leoworm <leo.charles.p@gmail.com>
2018-01-04 20:08:44 +0100

    init

---
+++ desktop/bin/untitled.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="road" tilewidth="32" tileheight="32" tilecount="8" columns="8">
+ <image source="road.png" width="256" height="32"/>
+</tileset>
